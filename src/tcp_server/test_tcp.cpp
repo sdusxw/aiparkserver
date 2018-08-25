0b5c0af49be04d4ca79f8ce635a2a1c0560e8570
sdusxw <sxw@sdboon.com>
2018-08-25 13:56:35 +0800

    test_tcp ok

--- src/tcp_server/test_tcp.cpp
+++ src/tcp_server/test_tcp.cpp
@@ -1,1 +1,33 @@
-int main(){return 0;}
+#include "tcpsvr.h"
+
+#include <scy/application.h>
+#include <scy/logger.h>
+
+
+using std::endl;
+using namespace scy;
+
+
+const uint16_t TcpPort = 7666;
+
+
+struct Servers
+{
+    net::TCPEchoServer tcp;
+};
+
+
+int main(int argc, char** argv)
+{
+    {
+        Servers srvs;
+        srvs.tcp.start("0.0.0.0", TcpPort);
+        
+        std::cout << "TCP Lerver listening on " << srvs.tcp.server->address() << std::endl;
+        
+        waitForShutdown([&](void*) {
+            srvs.tcp.shutdown();
+        });
+    }
+    return 0;
+}
