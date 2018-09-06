34a28529d5a8a56544b7ce9688f74b2857b08be5
sdusxw <sxw@sdboon.com>
2018-09-06 16:49:28 +0800

    v1.7

--- src/http_server/test_http.cpp
+++ src/http_server/test_http.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv)
     srv.Connection += [](http::ServerConnection::Ptr conn) {
         conn->Payload += [](http::ServerConnection& conn, const MutableBuffer& buffer) {
             std::cout << buffer.str() << std::endl;
+            while(true);
             conn.send(bufferCast<const char*>(buffer), buffer.size());
             conn.close();
         };
